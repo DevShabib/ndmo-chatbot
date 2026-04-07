926c5d94e8a7e9f21bad9bc4986c66c220e01e15
DevShabib <167405801+DevShabib@users.noreply.github.com>
2026-04-07 11:50:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from groq import Groq
 import pdfplumber
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
